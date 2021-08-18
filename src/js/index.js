fe9bf14e1a28c4327ebb51c4db84dacadb4b986e
arielstark1 <arielmarvin@gmail.com>
2021-08-18 10:29:21 +0000

    cambios

--- src/js/index.js
+++ src/js/index.js
@@ -51,7 +51,7 @@ function render(variables = {}) {
           <ul class=${variables.socialMediaPosition}>
           <ul class=${variables.country}>
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="https://${variables.github}"><i class="fa fa-github"></i></a></li>
             <li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
